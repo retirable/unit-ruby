afc73cdfbd15fd1c55385a935d33aa523c21f804
Kristina Shia <110191357+kristina-thatch@users.noreply.github.com>
2022-11-10 09:44:51 -0800

    add book payment resource with create

---
+++ lib/unit-ruby/book_payment.rb
@@ -0,0 +1,16 @@
+module Unit
+  class BookPayment < APIResource
+    path '/payments'
+
+    attribute :amount, Types::Integer
+    attribute :description, Types::String
+    attribute :transaction_summary_override, Types::String # Optional
+    attribute :tags, Types::Hash # Optional
+    attribute :idempotency_key, Types::String # Optional
+
+    belongs_to :account, resource_type: "depositAccount", class_name: 'Unit::DepositAccount'
+    belongs_to :counterparty_account, resource_type: "depositAccount", class_name: 'Unit::DepositAccount'
+
+    include ResourceOperations::Create
+  end
+end
