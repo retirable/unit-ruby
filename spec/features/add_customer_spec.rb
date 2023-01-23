2f4c31695a2d8ab3c913ca5607c23952dcb1d615
Michael Coleman <37200650+mcccoleman@users.noreply.github.com>
2023-01-23 12:22:37 -0500

    Add Deposit Account Feature Spec (#22)
    * add ability to add customer to deposit account
    
    * add gemfile

---
+++ spec/features/add_customer_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'securerandom'
+
+RSpec.describe Unit::DepositAccount do
+  before do
+    establish_connection_to_api!
+  end
+
+  let(:first_individual_customer) do
+    Factory.create_individual_customer
+  end
+
+  let(:second_individual_customer) do
+    Factory.create_individual_customer
+  end
+
+  let(:deposit_account) do
+    Unit::DepositAccount.create(
+      deposit_product: 'checking',
+      customer: first_individual_customer
+    )
+  end
+
+  it 'adds a new customer to the deposit account' do
+    account = deposit_account.add_customer(
+      second_individual_customer.id
+    )
+
+    expect(account.customers.length).to eq 2
+    expect(account.customers.map(&:id)).to eq [
+      first_individual_customer.id,
+      second_individual_customer.id
+    ]
+  end
+end
