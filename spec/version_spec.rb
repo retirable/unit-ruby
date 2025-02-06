f161008e024180056008ffbfffa37177dc8c8f9b
Trevor Nelson <trevor.nelson1@gmail.com>
2025-02-06 18:14:51 -0500

    Add TaxForm resource to fetch and download tax forms

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.describe Unit do
   it 'returns the correct version' do
-    expect(Unit::VERSION).to eq '0.10.0'
+    expect(Unit::VERSION).to eq '0.10.1'
   end
 end
 
